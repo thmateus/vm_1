a3e13b156518954db58e67eeb3d3abf65a28782f
Thiago Mateus <thiago_matheus_7@hotmail.com>
2021-04-14 18:10:03 -0300

    Adicionando projeto tests

---
+++ Codigo/TestSoftplan.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSoftplan.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
